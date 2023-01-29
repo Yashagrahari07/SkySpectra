023090ddd9d99d2c57c683dd7b855062ca7bbe93
Yash Agrahari <yashagrahari456@gmail.com>
2023-01-29 11:15:08 +0530

    Post Method and Changing Views

--- weather_app/views.py
+++ weather_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        city = request.POST.get('city')
+        # print(city)
+        api_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=ad6d91c541e7f84004e3201f55d89a05').read()
+        api_dict = json.loads(api_url)
+        print(api_dict)
     return render(request,'index.html',{})
