47085dafb704e3df1f4ce50f96cdd24382b6c74c
Yash Agrahari <yashagrahari456@gmail.com>
2023-01-29 11:28:49 +0530

    getting Weather Data from API in JSON Format

--- weather_app/views.py
+++ weather_app/views.py
@@ -7,7 +7,15 @@ def index(request):
     if request.method == 'POST':
         city = request.POST.get('city')
         # print(city)
-        api_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=ad6d91c541e7f84004e3201f55d89a05').read()
+        api_url = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=metric&appid=ecd55b265bce9fa821b3000b08b88256').read()
         api_dict = json.loads(api_url)
-        print(api_dict)
+        weather_data = {
+            "country": city,
+            "weather_description": api_dict['weather'][0]['description'],
+            "weather_temperature": api_dict['main']['temp'],
+            "weather_pressure": api_dict['main']['pressure'],
+            "weather_humidity": api_dict['main']['humidity'],
+            "weather_icon": api_dict['weather'][0]['icon'],
+        }
+        print(weather_data)
     return render(request,'index.html',{})
